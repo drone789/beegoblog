f3cd7011fdab2f9ad9ad7b364bac8c50dc3070bf
drone789 <drone789@163.com>
2016-07-26 14:13:18 +0800

    blog tags

---
+++ controllers/user.go
@@ -0,0 +1,33 @@
+package controllers
+
+import "jikeblog/models/class"
+
+type UserController struct {
+	//	beego.Controller
+	BaseController
+}
+
+func (c *UserController) Profile() {
+
+	id := c.Ctx.Input.Param(":id")
+	u := &class.User{Id: id}
+	u.ReadDB()
+
+	c.Data["u"] = u
+
+	a := &class.Article{Author: u}
+	as := a.Gets()
+
+	c.Data["articles"] = as
+
+	c.TplName = "user/profile.html"
+}
+
+func (c *UserController) PageJoin() {
+	c.TplName = "user/join.html"
+}
+
+func (c *UserController) PageSetting() {
+	c.CheckLogin()
+	c.TplName = "user/setting.html"
+}
